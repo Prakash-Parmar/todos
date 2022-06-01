c4f14928b6f55e021beb242fc0eea256b6a37110
Prakash-Parmar <80259553+Prakash-Parmar@users.noreply.github.com>
2022-06-01 10:50:47 +0530

    Add files via upload
    todos

---
+++ app.js
@@ -0,0 +1,51 @@
+const addform = document.querySelector('.add');
+const ul = document.querySelector('.todos');
+const search = document.querySelector('.search input');
+
+
+const generateTemplate = todo => {
+    const html =  `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${todo}</span>
+        <i class="far fa-trash-alt delete"></i>
+    </li>
+    `
+    ul.innerHTML += html;
+
+};
+
+addform.addEventListener('submit',e => {
+    e.preventDefault();
+    const todo = addform.add.value.trim();
+
+    if(todo.length){
+        generateTemplate(todo);
+        addform.reset();
+    }
+
+});
+
+ul.addEventListener('click', e => {
+    if(e.target.classList.toLowerCase().contains('delete')){
+        e.target.parentElement.remove();
+    }
+});
+
+const filterTodos = term => {
+    Array.from(ul.children)
+         .filter((t) => !t.textContent.includes(term))
+         .forEach((t) => t.classList.add('filtered'));
+
+    Array.from(ul.children)
+         .filter((t) => t.textContent.includes(term))
+         .forEach((t) => t.classList.remove('filtered'));
+};
+
+
+search.addEventListener('keyup', () => {
+    const term = search.value.trim().toLowerCase();
+    filterTodos(term); 
+});
+
+
+
